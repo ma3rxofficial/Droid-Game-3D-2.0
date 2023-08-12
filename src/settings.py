09445744a83be67b53848fc0d85714cb27f54ac7
Ma3rX <118477698+ma3rxofficial@users.noreply.github.com>
2023-08-12 18:02:19 +0300

    1.3.1

--- src/settings.py
+++ src/settings.py
@@ -3,7 +3,7 @@ import src.ipgetter as ipgetter
 # Настройки, информция
 
 NAME = 'Droid Game 3D'
-VERSION = 'Release 1.3' # версия
+VERSION = 'Release 1.3.1' # версия
 COPYRIGHT = 'Mark Kim 2021' # автор, год создания
 DEVELOP_MODE = False # не включаем режим разроботчика
 DEFAULT_IP = '127.0.0.1' # IP по умолчанию
